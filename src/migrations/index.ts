1a6a15aab1752f9ffa18e98fa8cb7d516c1c86ea
Developer One <developer1@coolmarketing.com>
2025-12-02 12:25:51 -0800

    feat(update): added npm run migration and added actual migration files

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251202_202446 from './20251202_202446';
+
+export const migrations = [
+  {
+    up: migration_20251202_202446.up,
+    down: migration_20251202_202446.down,
+    name: '20251202_202446'
+  },
+];
